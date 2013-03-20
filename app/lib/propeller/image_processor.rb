f7cfd92659f1ba76503c55ade168d22d15085e47
Piotr Żurkowski <kareth92@gmail.com>
2013-03-20 12:26:42 +0100

    Add some more yard documentation to image_processor and interface

--- app/lib/propeller/image_processor.rb
+++ app/lib/propeller/image_processor.rb
@@ -1,4 +1,6 @@
 class Propeller::ImageProcessor
+  # Change image from regular rectangular format to angular format
+  # @param path [String] path to orginal image
   def process path
   end
 end
--- app/lib/propeller/interface.rb
+++ app/lib/propeller/interface.rb
@@ -1,10 +1,14 @@
 class Propeller::Interface
+  # Shows preview and unlocks UI actions
+  # @param path [String] path to processed image
   def processed path
   end
 
+  # Hides the interface
   def hide
   end
 
+  # Shows the interface
   def show
   end
 end
