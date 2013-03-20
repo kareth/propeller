ab3f3e5c17ed4ba45610e3062e8be87b4ab2e043
Błażej Kotowski <blaziko@gmail.com>
2013-03-20 10:59:17 +0100

    hello world in shoes

--- app/lib/propeller.rb
+++ app/lib/propeller.rb
@@ -1,6 +1,6 @@
 class Propeller
   def self.start command, args
-    HelloWorld.say_it
+    HelloWorld.say_it_shoes
     puts "Command: '#{command}'"
     puts "Args: '#{args.join(", ")}'"
   end
--- app/lib/propeller/hello_world.rb
+++ app/lib/propeller/hello_world.rb
@@ -1,5 +1,17 @@
+require 'green_shoes'
+
 class Propeller::HelloWorld
   def self.say_it
     puts "hello world!"
+  end
+  
+  def self.say_it_shoes
+    Shoes.app(:title => "Awesome propeller", :width => 600, :height => 400) do
+      stack :margin => 10 do
+        button "Start propeller" do
+          alert "Hello World!"
+        end
+      end
+    end
   end
 end
