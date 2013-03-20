ef45330cc486be58f096b71bb7bb19bd0a48fc21
Piotr Żurkowski <kareth92@gmail.com>
2013-03-20 12:17:09 +0100

    Add basic yard documentation to Propeller class

--- app/lib/propeller.rb
+++ app/lib/propeller.rb
@@ -7,32 +7,46 @@ class Propeller
     @processor = ImageProcessor.new
   end
 
+  # Runs chosen command
+  # @note By far it ignores any command
+  # @note In the future, it should take some kind of communication information as args
   def run command, args
-    puts "Command: '#{command}'"
-    puts "Args: '#{args.join(", ")}'"
-
     @interface.show
+    #puts "Command: '#{command}'"
+    #puts "Args: '#{args.join(", ")}'"
   end
 
-  def image_loaded path, args = {}
+  # Notify processor about pending file to process
+  # @param path [String] path to selected raw image
+  # @param placement [Hash] placement of the image on the propeller display, including Width(w), Height(h), XOffset(x), YOffset(y)
+  def image_loaded path, placement = {}
     @processor.process path, args
   end
 
+  # Notify interface about finished image processing
+  # @param path [String] path to processed image
   def image_processed path
     @interface.processed path
   end
 
+  # Transmits information about selected image to propeller microprocessor
   def transmit
   end
 
+  # Stops the propeller machine
   def stop
   end
 
+  # Starts up the propeller machine
   def start
   end
 
+  # Exits the propeller program including:
+  # - stopping the propeller machine
+  # - closing the interface
   def exit
     @interface.hide
     # sth to destroy all dat shit??
+    # Shut down propeller
   end
 end
